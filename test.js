a3441dd9ca98d083b990fd0af6d827ce63509375
Micah Godbolt <micahgodbolt@gmail.com>
2017-01-12 14:12:21 -0800

    Updated tests

--- test.js
+++ test.js
@@ -6,17 +6,18 @@ describe('test page', function() {
    it('should look like test page', function () {
        browser.url('http://localhost:8080');
        if (fs.existsSync('baselines/myImage.png')) {
+           if (fs.existsSync('shots/myImage.png')) {
             fs.unlink('shots/myImage.png');  
-            browser.saveScreenshot('shots/myImage.png');
+           }
+            browser.saveElementScreenshot('shots/myImage.png', 'p');
             var baseline = fs.readFileSync('baselines/myImage.png');
             var shot = fs.readFileSync('shots/myImage.png');
-            resemble(baseline).compareTo(shot).onComplete(function(data){
+            var result = resemble(baseline).compareTo(shot).onComplete(function(data){
                 expect(data.misMatchPercentage).is.lessThan('0.05');
             });
-
         }
         else {
-            browser.saveScreenshot('./baselines/myImage.png');
+            browser.saveElementScreenshot('./baselines/myImage.png', 'p');
         }
        
 });
--- wdio.conf.js
+++ wdio.conf.js
@@ -20,7 +20,7 @@ exports.config = {
     // directory is where your package.json resides, so `wdio` will be called from there.
     //
     specs: [
-        'wdio.test.js'
+        'test.js'
     ],
     maxInstances: 10,
     //
